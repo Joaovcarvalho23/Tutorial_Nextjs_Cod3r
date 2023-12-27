f3d1325f4495326cfe96c441798ea4cd314434ad
Joao <joaovictorcordeiro00@gmail.com>
2023-12-27 11:33:14 -0300

    Criando página e componente de Caixa

--- app-inicial/src/app/basicos/caixa/page.tsx
+++ app-inicial/src/app/basicos/caixa/page.tsx
@@ -1,12 +1,17 @@
 import Caixa from "@/components/basicos/Caixa";
 
-export default function PaginaCaixa () {
+export default function PaginaCaixas () {
     return (
-        <div>
-            Segue abaixo os componetes de caixa:
-            <Caixa/>
-            <Caixa/>
-            <Caixa/>
+        <div className="flex gap-7 p-7">
+            <Caixa>Caixa 1</Caixa>
+            <Caixa>Caixa 2</Caixa>
+            <Caixa>
+                <ul className="list-disc">
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </Caixa>
         </div>
     )
 }
--- app-inicial/src/components/basicos/Caixa.tsx
+++ app-inicial/src/components/basicos/Caixa.tsx
@@ -1,7 +1,11 @@
-export default function Caixa () {
+export default function Caixa (props:any) {
     return (
-        <div>
-            Caixa
+        <div className={`
+            flex justify-center items-center
+            bg-purple-500 m-2 rounded-lg p-2
+            w-64 h-64 text-5xl font-sans 
+        `}>
+            {props.children}
         </div>
     )
 }
