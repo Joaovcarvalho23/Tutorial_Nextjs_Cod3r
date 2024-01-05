2fdabe4ad7a1c0b9641158b76b42d6425021b544
Joao <joaovictorcordeiro00@gmail.com>
2024-01-05 10:02:44 -0300

    add função do onClick nos botões

--- app-inicial/src/components/comunicacao/indireta/Botoes.tsx
+++ app-inicial/src/components/comunicacao/indireta/Botoes.tsx
@@ -1,13 +1,15 @@
 interface BotoesProps{
+    inc: () => void
+    dec: () => void
 }
 
 export default function Botoes (props: BotoesProps) {
     return (
-        <div className="flex justify-between pt-2">
-            <button className="botaoInc">
+        <div className="flex justify-between pt-2 gap-2">
+            <button className="botaoInc flex-1" onClick={props.inc}>
                 Incrementar
             </button>
-            <button className="botaoDec">
+            <button className="botaoDec flex-1" onClick={props.dec}>
                 Decrementar
             </button>
         </div>
