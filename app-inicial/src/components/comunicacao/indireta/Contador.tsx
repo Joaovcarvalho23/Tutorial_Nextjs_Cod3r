9edd348069b572a7cc45c4127c6946a327b0bb80
Joao <joaovictorcordeiro00@gmail.com>
2024-01-05 10:03:04 -0300

    implementando funções para operações de inc e dec

--- app-inicial/src/components/comunicacao/indireta/Contador.tsx
+++ app-inicial/src/components/comunicacao/indireta/Contador.tsx
@@ -6,12 +6,21 @@ import Display from "./Display";
 export default function Contador () {
     const [num, setNum] = useState<number>(0)
     
+    let incrementar = () => {
+        setNum(num + 1)
+    }
+
+    let decrementar = () => {
+        setNum(num - 1)
+    }
+    
     return (
         <div className={`
             flex flex-col p-2 border border-red-600 rounded-lg
+            w-72 h-72
         `}>
-            <Display valor={32} />
-            <Botoes />
+            <Display valor={num} />
+            <Botoes inc={incrementar} dec={decrementar}/>
         </div>
     )
 }
