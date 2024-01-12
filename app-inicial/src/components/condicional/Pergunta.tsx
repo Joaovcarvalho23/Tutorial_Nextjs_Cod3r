acc8b8794a65927b3be17f9abf2cbe876b188778
Joao <joaovictorcordeiro00@gmail.com>
2024-01-12 10:37:34 -0300

    fazendo a estrutura do componente Pergunta

--- app-inicial/src/components/condicional/Pergunta.tsx
+++ app-inicial/src/components/condicional/Pergunta.tsx
@@ -1,7 +1,41 @@
-export default function Pergunta(){
+"use client"
+import { useState } from "react"
+import If from "./If"
+
+interface PerguntaProps {
+    texto: string
+    alternativas: string
+    resposta: string
+}
+
+export default function Pergunta(props: PerguntaProps){
+    const [aberta, setAberta] = useState<boolean>(false)
+
     return (
-        <div>
-            Esta é uma pergunta!
+        <div className={`
+            border border-white rounded-md
+            overflow-hidden
+        `}>
+            <div 
+                className="bg-zinc-700 p-5 cursor-pointer select-none"
+                onClick={() => setAberta(!aberta)}
+            >
+                {props.texto}
+                {props.alternativas}
+            </div>
+            {/*{aberta? "Sim" : "Não"}*/}
+            {/*{aberta && "Pergunta aberta"}*/}
+            {/*{aberta && (
+                <div className="bg-green-600 p-5">
+                    {props.resposta}
+                </div>
+            )}*/}
+
+            <If teste={aberta}>
+                <div className="bg-green-600 p-5">
+                    {props.resposta}
+                </div>
+            </If>
         </div>
     )
 }
