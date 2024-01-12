d2cbf9ff3ba8112b2c471b7e727a9325e7d0042f
Joao <joaovictorcordeiro00@gmail.com>
2024-01-12 10:37:09 -0300

    estruturando a FAQ

--- app-inicial/src/components/condicional/Faq.tsx
+++ app-inicial/src/components/condicional/Faq.tsx
@@ -2,8 +2,30 @@ import Pergunta from "./Pergunta";
 
 export default function Faq() {
     return (
-        <div>
-            <Pergunta />
+        <div className="flex flex-col gap-3 w-[90%] md:w-3/4">
+            <Pergunta 
+                texto="De quem é a famosa frase “Penso, logo existo”?"
+                alternativas=""
+                resposta="Descartes"
+            />
+
+            <Pergunta
+                texto="De onde é a invenção do chuveiro elétrico?"
+                alternativas=""
+                resposta="Brasil"
+            />
+
+            <Pergunta
+                texto="Quais o menor e o maior país do mundo?"
+                alternativas=""
+                resposta="Vaticano e Rússia"
+            />
+
+            <Pergunta
+                texto="Qual o nome do presidente do Brasil que ficou conhecido como Jango?"
+                alternativas=""
+                resposta="João Goulart"
+            />
         </div>
     )
 }
