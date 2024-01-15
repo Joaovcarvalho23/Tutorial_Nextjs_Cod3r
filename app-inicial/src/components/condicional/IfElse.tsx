d5d72bcf90a565ace6da6ddb70012ab52e1dff91
Joao <joaovictorcordeiro00@gmail.com>
2024-01-15 09:30:47 -0300

    implementando componente do IfElse

--- app-inicial/src/components/condicional/IfElse.tsx
+++ app-inicial/src/components/condicional/IfElse.tsx
@@ -1,7 +1,13 @@
-export default function IfElse (){
-    return(
-        <div>
-            
-        </div>
-    )
+interface IfElseProps {
+    teste: boolean
+    children: any
+}
+
+export default function IfElse (props: IfElseProps){
+    if(props.teste){
+        return props.children[0]
+    
+    } else {
+        return props.children[1]
+    }
 }
